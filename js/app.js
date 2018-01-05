639beae4e2cfd637eac27e6b4391ff38e005cae2
Berdia <berdia.romelashvili@geolab.edu.ge>
2018-01-05 19:36:01 +0400

    Update 05.01
    update 05.01

--- js/app.js
+++ js/app.js
@@ -14,3 +14,15 @@ $('.menu-toggle').click(function() {
    $('.right_control').toggleClass('hide');      
 
 })
+
+function initMap() {
+        var uluru = {lat: 41.71751392, lng: 44.78465617};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
